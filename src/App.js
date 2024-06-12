07d06063f15d4656b21c794158c6c8f723852bcf
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-06-12 15:17:06 +0530

    Initial Commit :- Setup create react app and Configured Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+     Hello Namaste React , Netflix GPT this time 
+    </div>
+  );
+}
+
+export default App;
