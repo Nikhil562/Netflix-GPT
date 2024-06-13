6773fd565d1cd3d32ae236f8e9dbc410835a9adb
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-06-13 23:23:14 +0530

    Added bg and logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <div className='px-8 py-2 bg-gradient-to-b from-black'>
+        <img  className="w-36"
+        src="https://github.com/piyush805/Namaste_NetflixGPT/blob/1229a3bf037a175aa574184b9e5cbe62bc50a380/src/images/Logonetflix.png?raw=true" alt="logo" />
+
+  </div>
 }
 
 export default Header
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Header from './Header'
 const Login = () => {
-  return (
-    <div>Login HI THere </div>
-  )
+  return <div>
+    <Header/>
+    <div><img src="https://www.pcworld.com/wp-content/uploads/2024/05/Netflix-Hintergrund-1.jpg?resize=1240%2C697&quality=50&strip=all" alt="logo" /></div>
+  </div>
 }
 
 export default Login
