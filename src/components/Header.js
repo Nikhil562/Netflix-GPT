ef83bd468fa2191d38b1abdf131d712559a7d6df
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-07-08 14:57:11 +0530

    Created Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = () => {
-  return <div className='px-8 py-2 bg-gradient-to-b from-black'>
+  return <div className='px-8 py-2 bg-gradient-to-b from-black z-10'>
         <img  className="w-36"
         src="https://github.com/piyush805/Namaste_NetflixGPT/blob/1229a3bf037a175aa574184b9e5cbe62bc50a380/src/images/Logonetflix.png?raw=true" alt="logo" />
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from './Header'
 const Login = () => {
-  return <div>
+  return (
+  <div>
     <Header/>
-    <div><img src="https://www.pcworld.com/wp-content/uploads/2024/05/Netflix-Hintergrund-1.jpg?resize=1240%2C697&quality=50&strip=all" alt="logo" /></div>
+    <div className='absolute'>
+        <img src="https://www.pcworld.com/wp-content/uploads/2024/05/Netflix-Hintergrund-1.jpg?resize=1240%2C697&quality=50&strip=all" alt="logo" />
+    </div>
+    <form className='w-3/12 relative p-12 bg-black'>
+        <input type="text" placeholder='Email Address' className='p-2 m-2 '/>
+        <input type="password" placeholder='Password' className='p-2 m-2 '/>
+        <button className="p-4 m-4">Sign In </button>
+    </form>
   </div>
-}
+  );
+};
 
 export default Login
